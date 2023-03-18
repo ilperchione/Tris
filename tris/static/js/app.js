338ae48864e7745a5fdef19ebbbdf5855f6b5b56
ilperchione <116425725+ilperchione@users.noreply.github.com>
2023-03-18 09:47:48 +0100

    riaggiornamento js
    sto rifacendo tutto js perché non ho salvato su github e ho perso tutto, porca di quella...

--- tris/static/js/app.js
+++ tris/static/js/app.js
@@ -14,9 +14,25 @@ cellIsOccupied = true
 			cellIsOccupied = e.currentTarget.classList.contains("player2") || e.currentTarget.classList.contains("player1")	;			
 			console.log(cellIsOccupied)
 					
-			if(!cellIsOccupied){
+			if(!cellIsOccupied || !finish){
 				e.currentTarget.classList.toggle(`player${currentPlayer}`)
 				currentPlayer = currentPlayer === 1 ? 2 : 1
+			}
+			else if(checkOrizzontale() === true){
+				finish = true
+				win.innerHTML=`ha vinto il giocatore ${currentPlayer}`;
+			}
+			else if(checkVerticale() === true){
+				finish = true
+				win.innerHTML=`ha vinto il giocatore ${currentPlayer}`;
+			}
+			else if(checkDiagonale() === true){
+				finish = true
+				win.innerHTML=`ha vinto il giocatore ${currentPlayer}`;
+			}
+			else if(checkPareggio() === true){
+				finish = true
+				win.innerHTML=`i giocatori hanno pareggiato`;
 			}
 		})
 	}
@@ -68,9 +84,35 @@ function ceckDiagonale(currentPlayer){
 			return true;
 	}
 
-	else if(grid[2][1] === currentPlayer && 
+	else if(grid[0][2] === currentPlayer && 
 			grid[1][1] === currentPlayer &&
 			grid[2][0] === currentPlayer ){
 				return true;
 	}
 }
+
+function checkPareggio(){
+	if(
+		grid[0][0] != null &&
+		grid[0][1] != null &&
+		grid[0][2] != null &&
+		grid[1][0] != null &&
+		grid[1][1] != null &&
+		grid[1][2] != null &&
+		grid[2][0] != null &&
+		grid[2][1] != null &&
+		grid[2][2] != null){
+			return true;
+		}
+}
+
+let restart = document.querySelector(".restart")
+		restart.addEventListener("click",
+			function(e){
+				e.preventDefault()
+				grid = [
+					[null, null, null],
+					[null, null, null],
+					[null, null, null]
+				]
+			})
