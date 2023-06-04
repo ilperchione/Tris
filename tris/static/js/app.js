b9c1c35aebc6863e41566996ecf34fade0e34a44
Michele Palumbo <116425725+ilperchione@users.noreply.github.com>
2023-06-04 17:38:18 +0200

    player VS player terminato

--- tris/static/js/app.js
+++ tris/static/js/app.js
@@ -1,118 +1,139 @@
-let links = document.querySelectorAll(".gridBtn")
+let links = document.querySelectorAll(".gridBtn");
 let grid = [
 	[null, null, null],
 	[null, null, null],
 	[null, null, null]
-]
-currentPlayer = 1,
-cellIsOccupied = true
+];
+let currentPlayer = 1;
+let finish = false;
 
-	for(let i = 0; i < links.length; i++){
-		let link = links[i]
-		link.addEventListener("click", function(e){
-			e.preventDefault()
-			cellIsOccupied = e.currentTarget.classList.contains("player2") || e.currentTarget.classList.contains("player1")	;			
-			console.log(cellIsOccupied)
-					
-			if(!cellIsOccupied || !finish){
-				e.currentTarget.classList.toggle(`player${currentPlayer}`)
-				currentPlayer = currentPlayer === 1 ? 2 : 1
-			}
-			else if(checkOrizzontale() === true){
-				finish = true
-				win.innerHTML=`ha vinto il giocatore ${currentPlayer}`;
-			}
-			else if(checkVerticale() === true){
-				finish = true
-				win.innerHTML=`ha vinto il giocatore ${currentPlayer}`;
-			}
-			else if(checkDiagonale() === true){
-				finish = true
-				win.innerHTML=`ha vinto il giocatore ${currentPlayer}`;
-			}
-			else if(checkPareggio() === true){
-				finish = true
-				win.innerHTML=`i giocatori hanno pareggiato`;
-			}
-		})
-	}
-
-function checkOrizzontale(currentPlayer){
-	if(	grid[0][0] === currentPlayer && 
-		grid[0][1] === currentPlayer &&
-		grid[0][2] === currentPlayer ){
-			return true;
-	}
-
-	else if(grid[1][0] === currentPlayer && 
-			grid[1][1] === currentPlayer &&
-			grid[1][2] === currentPlayer ){
-				return true;
-	}
-
-	else if(grid[2][0] === currentPlayer && 
-			grid[2][1] === currentPlayer &&
-			grid[2][2] === currentPlayer ){
-				return true;
-	}
-}
-
-function checkVerticale(currentPlayer){
-	if(	grid[0][0] === currentPlayer && 
-		grid[1][0] === currentPlayer &&
-		grid[2][0] === currentPlayer ){
-			return true;
-	}
-
-	else if(grid[0][1] === currentPlayer && 
-			grid[1][1] === currentPlayer &&
-			grid[2][1] === currentPlayer ){
-				return true;
-	}
-
-	else if(grid[0][2] === currentPlayer && 
-			grid[1][2] === currentPlayer &&
-			grid[2][2] === currentPlayer ){
-				return true;
-	}
-}
-
-function ceckDiagonale(currentPlayer){
-	if(	grid[0][0] === currentPlayer && 
-		grid[1][1] === currentPlayer &&
-		grid[2][2] === currentPlayer ){
-			return true;
-	}
-
-	else if(grid[0][2] === currentPlayer && 
-			grid[1][1] === currentPlayer &&
-			grid[2][0] === currentPlayer ){
-				return true;
-	}
-}
-
-function checkPareggio(){
-	if(
-		grid[0][0] != null &&
-		grid[0][1] != null &&
-		grid[0][2] != null &&
-		grid[1][0] != null &&
-		grid[1][1] != null &&
-		grid[1][2] != null &&
-		grid[2][0] != null &&
-		grid[2][1] != null &&
-		grid[2][2] != null){
-			return true;
+for (let i = 0; i < links.length; i++) {
+	let link = links[i];
+	link.addEventListener("click", function (e) {
+	e.preventDefault();
+	let cellIsOccupied =
+		e.currentTarget.classList.contains("player2") ||
+		e.currentTarget.classList.contains("player1");
+	if (!cellIsOccupied && !finish) {
+		e.currentTarget.classList.toggle(`player${currentPlayer}`);
+		let row = e.currentTarget.getAttribute("data-row");
+		let column = e.currentTarget.getAttribute("data-column");
+		grid[row][column] = currentPlayer;
+		if (checkOrizzontale(currentPlayer)) {
+		finish = true;
+		let win = document.querySelector(".victory h2");
+		win.innerHTML = `Ha vinto il giocatore ${currentPlayer}`;
+		document.querySelector(".victory").classList.remove("hidden");
+		} else if (checkVerticale(currentPlayer)) {
+		finish = true;
+		let win = document.querySelector(".victory h2");
+		win.innerHTML = `Ha vinto il giocatore ${currentPlayer}`;
+		document.querySelector(".victory").classList.remove("hidden");
+		} else if (checkDiagonale(currentPlayer)) {
+		finish = true;
+		let win = document.querySelector(".victory h2");
+		win.innerHTML = `Ha vinto il giocatore ${currentPlayer}`;
+		document.querySelector(".victory").classList.remove("hidden");
+		} else if (checkPareggio()) {
+		finish = true;
+		let win = document.querySelector(".victory h2");
+		win.innerHTML = `I giocatori hanno pareggiato`;
+		document.querySelector(".victory").classList.remove("hidden");
+		} else {
+		currentPlayer = currentPlayer === 1 ? 2 : 1;
 		}
+	}
+	});
 }
 
-let restart = document.querySelector(".restart")
-		restart.addEventListener("click",
-			function(e){
-				e.preventDefault()
-				grid = [
-					[null, null, null],
-					[null, null, null],
-					[null, null, null]
-				]
-			})
+function checkOrizzontale(currentPlayer) {
+	if (
+	grid[0][0] === currentPlayer &&
+	grid[0][1] === currentPlayer &&
+	grid[0][2] === currentPlayer
+	) {
+	return true;
+	} else if (
+	grid[1][0] === currentPlayer &&
+	grid[1][1] === currentPlayer &&
+	grid[1][2] === currentPlayer
+	) {
+	return true;
+	} else if (
+	grid[2][0] === currentPlayer &&
+	grid[2][1] === currentPlayer &&
+	grid[2][2] === currentPlayer
+	) {
+	return true;
+	}
+	return false;
+}
+
+function checkVerticale(currentPlayer) {
+	if (
+	grid[0][0] === currentPlayer &&
+	grid[1][0] === currentPlayer &&
+	grid[2][0] === currentPlayer
+	) {
+	return true;
+	} else if (
+	grid[0][1] === currentPlayer &&
+	grid[1][1] === currentPlayer &&
+	grid[2][1] === currentPlayer
+	) {
+	return true;
+	} else if (
+	grid[0][2] === currentPlayer &&
+	grid[1][2] === currentPlayer &&
+	grid[2][2] === currentPlayer
+	) {
+	return true;
+	}
+	return false;
+}
+
+function checkDiagonale(currentPlayer) {
+	if (
+	grid[0][0] === currentPlayer &&
+	grid[1][1] === currentPlayer &&
+	grid[2][2] === currentPlayer
+	) {
+	return true;
+	} else if (
+	grid[0][2] === currentPlayer &&
+	grid[1][1] === currentPlayer &&
+	grid[2][0] === currentPlayer
+	) {
+	return true;
+	}
+	return false;
+}
+
+function checkPareggio() {
+	for (let row = 0; row < grid.length; row++) {
+	for (let col = 0; col < grid[row].length; col++) {
+		if (grid[row][col] === null) {
+		return false;
+		}
+	}
+	}
+	return true;
+}
+
+let restart = document.querySelector(".restart");
+restart.addEventListener("click", function (e) {
+	e.preventDefault();
+	grid = [
+	[null, null, null],
+	[null, null, null],
+	[null, null, null]
+	];
+	currentPlayer = 1;
+	finish = false;
+	document.querySelector(".victory").classList.add("hidden");
+	let cells = document.querySelectorAll(".gridBtn");
+	for (let i = 0; i < cells.length; i++) {
+	cells[i].classList.remove("player1", "player2");
+	}
+});
+
